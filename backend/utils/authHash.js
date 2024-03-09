f84a6b86f5e84697d4ad26d3505fa24251ea8558
borkarSahil <officialsahilborkar@gmail.com>
2024-03-09 15:51:41 +0530

    Hashesd Password/Verify : bcrypt

---
+++ backend/utils/authHash.js
@@ -0,0 +1,17 @@
+import bcrypt from "bcryptjs";
+
+export const hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
+  } catch (error) {
+    console.log("Hash password error: " + error);
+  }
+};
+
+
+export const comparePassword = async (password, bcryptPassword) => {
+    return bcrypt.compare(password ,bcryptPassword );
+}

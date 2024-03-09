b74f95d1159edf2282990d0903d70e08631f7a06
borkarSahil <officialsahilborkar@gmail.com>
2024-03-09 15:48:08 +0530

    Generate Token : JWT

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -5,16 +5,7 @@ const generateToken = (res, userId) => {
     expiresIn: "30d",
   });
 
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-  });
-
-
   return token;
 };
-
 
 export default generateToken;

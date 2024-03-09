3a2e02d62acdb71196c321170170627dd034ac8d
borkarSahil <officialsahilborkar@gmail.com>
2024-03-09 15:50:43 +0530

    Product Routes / Controllers / Model

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import {
+  createProductController,
+  getProductController,
+  getProductByIdController,
+  getPhotoByIdController,
+  deleteByIdController,
+  updateProductController,
+} from "../controllers/productController.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+router.post(
+  "/create-product",
+  authenticate,
+  authorizeAdmin,
+  formidable(),
+  createProductController
+);
+
+router.get("/get-products", getProductController);
+
+router.get("/get-products", getProductController);
+
+router.get("/get-products/:id", getProductByIdController);
+
+router.get("/get-photo/:id", getPhotoByIdController);
+
+router.delete("/delete-product/:id", deleteByIdController);
+
+
+router.put(
+  "/update-product/:id",
+  authenticate,
+  authorizeAdmin,
+  formidable(),
+  updateProductController
+);
+
+export default router;

ee62d8db44f410c6b364b6d737c1d5e716f80c4d
borkarSahil <officialsahilborkar@gmail.com>
2024-05-30 14:22:55 +0530

    Context and Helpers

---
+++ client/src/helper/Prices.js
@@ -0,0 +1,27 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "0 - 10",
+    array: [0, 10],
+  },
+  {
+    _id: 1,
+    name: "10 - 20",
+    array: [10, 20],
+  },
+  {
+    _id: 2,
+    name: "20 - 30",
+    array: [20, 30],
+  },
+  {
+    _id: 4,
+    name: "30 - 40",
+    array: [30, 40],
+  },
+  {
+    _id: 5,
+    name: "Above 40",
+    array: [40, 9999],
+  }
+];

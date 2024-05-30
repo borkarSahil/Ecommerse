0beb0b8bc868f1690b7b2a2ecfbc7ca62865dc03
borkarSahil <officialsahilborkar@gmail.com>
2024-05-30 14:16:34 +0530

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,12 +6,18 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "./Context/auth.jsx";
 import "antd/dist/reset.css";
+import { SearchProvider } from "./Context/search.jsx";
+import { CartProvider } from "./Context/cart.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <BrowserRouter>
-      <ToastContainer />
-      <App />
-    </BrowserRouter>
+    <SearchProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <ToastContainer />
+          <App />
+        </BrowserRouter>
+      </CartProvider>
+    </SearchProvider>
   </AuthProvider>
 );
